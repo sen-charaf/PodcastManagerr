22b55c6bb92ce64b3c59c3388448df3a63645f6b
Charaf <Charaf@gmail.com>
2023-12-20 19:30:08 +0100

    Acceuil Finished + create HomePage file

--- application/AcceuilController.java
+++ application/AcceuilController.java
@@ -1,13 +1,26 @@
 package application;
 
+
+import java.io.IOException;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 
 public class AcceuilController {
-	
-   
+
+    @FXML
+    private FontAwesomeIcon arrowID;
+
     @FXML
     private AnchorPane welcomeBG;
 
@@ -20,8 +33,25 @@ public class AcceuilController {
     @FXML
     private Label wolcomeLabel1;
     
-    public void switchtohovercolor() {
-    	
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    
+    public void Enter_changetohoveredCircle(MouseEvent  event) {
+    	welcomeCirclBTN.setStyle("-fx-fill: #8A62C9E5;");
+ 
     }
+    public void Leave_changetohoveredCircle(MouseEvent  event) {
+    	welcomeCirclBTN.setStyle("-fx-fill: #783ed4;");
+    }
+    public void ClickStartBTN(MouseEvent  event) throws IOException {
+    	System.out.println("1");
+    	Parent root = FXMLLoader.load(getClass().getResource("/HomePage.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+    
 
 }
--- module-info.java
+++ module-info.java
@@ -2,6 +2,8 @@ module Podcast_GS {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
+	requires fontawesomefx;
+	requires javafx.base;
 	
 	
 	opens application to javafx.graphics, javafx.fxml;
